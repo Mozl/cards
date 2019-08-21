7539600a2b188bf67b53590173a20c861fc9ac5e
Louis Moselhi <louis.moselhi@bbc.co.uk>
2019-08-21 16:08:40 +0100

    remove old increment code

--- src/index.js
+++ src/index.js
@@ -7,35 +7,4 @@ import { Provider } from 'react-redux';
 
 let store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-// const increment = () => {
-//   return {
-//     type: 'INCREMENT'
-//   }
-// }
-
-// const decrement = () => {
-//   return {
-//     type: 'DECREMENT'
-//   }
-// }
-
-// //REDUCER
-
-// const counter = (state = 0, action) => {
-//   switch(action.type) {
-//     case 'INCREMENT':
-//       return state + 1;
-//     case 'DECREMENT':
-//       return state - 1;
-//     default:
-//       return state;
-//   }
-// }
-
-// let store = createStore(counter);
-
-// store.subscribe(() => console.log(store.getState()))
-
-// store.dispatch(increment())
-
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
